a13c2fd763b2eaff13edfbbb608f619e22aacd00
lucas <lucasriibeiro23@gmail.com>
2023-06-28 12:00:33 -0300

    verificando requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -3,8 +3,8 @@ export const searchCities = async (term) => {
   const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
   const data = await response.json();
 
-  if (!term) {
-    window.alert('Nenhuma cidade encontrada')
+  if (!term || data.length === 0) {
+    window.alert('Nenhuma cidade encontrada');
   }
 
   return data;
